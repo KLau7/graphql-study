6693af14ee09355134639627e2a022f51fb8a91b
Leon Lau <leon.kw.lau@gtomato.com>
2022-12-07 17:57:49 +0800

    job board init

--- server/server.js
+++ server/server.js
@@ -1,17 +1,30 @@
-import { ApolloServer, gql } from 'apollo-server';
+import cors from 'cors';
+import express from 'express';
+import { expressjwt } from 'express-jwt';
+import jwt from 'jsonwebtoken';
+import { User } from './db.js';
 
-const typeDefs = gql`
-    type Query {
-        greeting: String
-    }
-`;
+const PORT = 9000;
+const JWT_SECRET = Buffer.from('Zn8Q5tyZ/G1MHltc4F/gTkVJMlrbKiZt', 'base64');
 
-const resolvers = {
-    Query: {
-        greeting: () => 'Hello World',
-    }
-}
+const app = express();
+app.use(cors(), express.json(), expressjwt({
+  algorithms: ['HS256'],
+  credentialsRequired: false,
+  secret: JWT_SECRET,
+}));
 
-const server = new ApolloServer({ typeDefs, resolvers });
-const { url } = await server.listen({ port: 9000 });
-console.log(`Server running at ${url}`);
+app.post('/login', async (req, res) => {
+  const { email, password } = req.body;
+  const user = await User.findOne((user) => user.email === email);
+  if (user && user.password === password) {
+    const token = jwt.sign({ sub: user.id }, JWT_SECRET);
+    res.json({ token });  
+  } else {
+    res.sendStatus(401);
+  }
+});
+
+app.listen({ port: PORT }, () => {
+  console.log(`Server running on port ${PORT}`);
+});
